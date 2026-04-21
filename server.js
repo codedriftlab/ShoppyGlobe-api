2b9e7417d74c79f761f665a496ecc7edda9be360
codedriftlab <tempmail3470@gmail.com>
2026-04-21 15:20:41 +0530

    server file create

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+// connect DB
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("DB Connected"))
+.catch(err=>console.log(err));
+
+// routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/cart", require("./routes/cartRoutes"));
+
+// server
+app.listen(3000, ()=>console.log("Server running"));
